24f2e2cd92f8a8e519c0344ec1d81ffbb80721f6
Stephanie Huang <stxphcodes@stephanies-mbp.mynetworksettings.com>
2023-10-12 11:16:48 -0400

    Fix get server logic

--- ui/apis/get_server.ts
+++ ui/apis/get_server.ts
@@ -1,15 +1,15 @@
 export function GetServerURL(clientSide?: boolean) {
-    if (clientSide && process.env.NODE_ENV == "development") {
-        return "http://localhost:8000"
-    }
-
-    if (process.env.NEXT_PUBLIC_SERVER_URL ) {
+    if (process.env.NODE_ENV == "production") {
         return process.env.NEXT_PUBLIC_SERVER_URL
     }
 
     if (process.env.NODE_ENV == "development") {
-        return "http://localhost:8000"
+        if (clientSide) {
+            return "http://localhost:8000"
+        }
+
+        return process.env.NEXT_PUBLIC_SERVER_URL
     }
 
-    return window.location.href
+   return process.env.NEXT_PUBLIC_SERVER_URL
 }
