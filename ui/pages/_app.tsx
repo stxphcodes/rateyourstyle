5388aa439c32ffe6f338544e40e58928d3645820
Stephanie Huang <stxphcodes@stephanies-mbp.mynetworksettings.com>
2023-11-14 21:03:53 -0500

    UseRouter to fix pages not reloading

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,15 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from "next/head";
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // See this github issue on how using router helps with 
+  // page reloading issue
+  // https://github.com/vercel/next.js/discussions/22512#discussioncomment-1779505
+  const router = useRouter()
+
   return (
     <>
       <Head>
         <title>RateYourStyle</title>
         <link rel="icon" type="image/x-icon" href="/rys.png"></link>
       </Head>
-      <Component {...pageProps} />
+      <Component {...pageProps} key={router.asPath}/>
     </>
   )
 }
