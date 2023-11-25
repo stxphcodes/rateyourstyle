dc2a85038f72d8811cb02c524bd07e5f380fdf35
Steph <33664901+stxphcodes@users.noreply.github.com>
2023-11-25 06:03:49 -0500

    Issue-39: Improve site SEO (#41)
    * Add page metadata
    
    * Update sitemap.xml
    
    ---------
    
    Co-authored-by: stxphcodes <sitesbystephanie@gmail.com>

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -3,17 +3,31 @@ import type { AppProps } from 'next/app'
 import Head from "next/head";
 import { useRouter } from 'next/router'
 
-function MyApp({ Component, pageProps }: AppProps) {
+export type PageMetadata = {
+  title: string;
+  description: string;
+};
+
+function MyApp({ Component, pageProps }: AppProps<any>) {
   // See this github issue on how using router helps with 
   // page reloading issue
   // https://github.com/vercel/next.js/discussions/22512#discussioncomment-1779505
-  const router = useRouter()
+  const router = useRouter();
+  let metadata = pageProps.metadata;
 
   return (
     <>
       <Head>
-        <title>RateYourStyle</title>
+        <title>{metadata && metadata.title ?  metadata.title + " - Rate Your Style" : "Rate Your Style"}</title>
         <link rel="icon" type="image/x-icon" href="/rys.png"></link>
+        <meta
+          name="description"
+          content={
+           metadata && metadata.description
+              ? metadata.description
+              : "Rate Your Style is an online fashion community for all of your style inspo needs. Post outfit pics to get fashion feedback, and give style advice to other users. Find new clothing brands and read reviews about each clothing item. Keep an e-inventory of all of your clothes through our closet table feature."
+          }
+        />
       </Head>
       <Component {...pageProps} key={router.asPath}/>
     </>
