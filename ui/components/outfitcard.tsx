7b25e953653b1ae95e85dbb9d5c5e9ab571db020
stxphcodes <sitesbystephanie@gmail.com>
2023-12-17 21:26:44 -0500

    Make outfit card more consistent

--- ui/components/outfitcard.tsx
+++ ui/components/outfitcard.tsx
@@ -5,6 +5,7 @@ import { GetRatings, GetRatingsByOutfit, Rating } from '../apis/get_ratings';
 import { Modal } from './modals';
 import { PostRating } from '../apis/post_rating';
 import { VerifiedCheckIcon } from './icons/verified-check-icon';
+import Image from 'next/image'
 
 
 function Rating(props: { x: number, small?: boolean }) {
@@ -38,6 +39,7 @@ export function OutfitCard(props: {
 
 	const [allRatings, setAllRatings] = useState<Rating[] | null>(null)
 	const [readMore, setReadMore] = useState(false)
+
 
 	const handleSubmitRating = async (e: any) => {
 		e.preventDefault();
@@ -79,30 +81,28 @@ export function OutfitCard(props: {
 	return (
 		<>
 			<div className="w-72 shadow-md break-words">
-				<div className="px-2 py-1 bg-background text-sm " style={{fontFamily: 'custom-serif'}}>
-					{props.data.username ? <a className="flex flex-wrap items-center gap-1" href={`/closet/${props.data.username}`}>{props.data.username}&apos;s closet {props.verifiedBusiness && <VerifiedCheckIcon small={true}/>}</a> : "anonymous"}
-					
-					
+				<div className="px-2 py-1 bg-background text-sm " style={{ fontFamily: 'custom-serif' }}>
+					{props.data.username ? <a className="flex flex-wrap items-center gap-1" href={`/closet/${props.data.username}`}>{props.data.username}&apos;s closet {props.verifiedBusiness && <VerifiedCheckIcon small={true} />}</a> : "anonymous"}
+
+
 					<div className="">{props.data.date}</div>
 				</div>
+				<div className="relative   h-96 " onClick={() => setExpandImage(true)}>
+					<Image
 
-				<img
-					onClick={() => setExpandImage(true)}
-					className="object-contain hover:cursor-pointer"
-					src={props.data.picture_url}
-				/>
-
+						alt={props.data.title}
+						src={props.data.picture_url}
+						layout="fill"
+						objectFit="cover"
+						sizes="(max-width: 768px) 60vw, 25vw"
+					/>
+				</div>
 				<div className="p-2">
-					<h6 className="">{props.data.title}</h6>
-					<div className="flex gap-2 flex-wrap flex-row">
-						{props.data.style_tags.map((item) => (
-							<div className="" key={item}>{item}</div>
-						))}
-					</div>
+					<div style={{ fontFamily: 'custom-serif' }} className="whitespace-nowrap text-ellipsis overflow-hidden">{props.data.title}</div>
 					<div className="flex items-center mt-1">
-							<div className="hover:cursor-pointer mr-2" onClick={() => setExpandImage(true)}>
-								<Rating x={props.data.rating_average ? props.data.rating_average : 0} small={true}/>
-							</div>
+						<div className="hover:cursor-pointer mr-2" onClick={() => setExpandImage(true)}>
+							<Rating x={props.data.rating_average ? props.data.rating_average : 0} small={true} />
+						</div>
 						<a className="" onClick={() => setExpandImage(true)}>
 							{
 								!props.data.rating_count ? "no reviews submitted yet" : `from ${props.data.rating_count} ${props.data.rating_count > 1 ? "reviews" : "review"} `
@@ -131,7 +131,6 @@ export function OutfitCard(props: {
 							}}>View less</a>
 						</div>
 					</>
-
 				}
 
 			</div>
@@ -171,7 +170,7 @@ export function OutfitCard(props: {
 								}
 
 								<div className="flex items-center">
-									<Rating x={props.data.rating_average ? props.data.rating_average : 0}/>
+									<Rating x={props.data.rating_average ? props.data.rating_average : 0} />
 									<div className="mx-2">
 										{
 											!props.data.rating_count ? "no reviews submitted yet" : `from ${props.data.rating_count} ${props.data.rating_count > 1 ? "reviews" : "review"} `
@@ -268,8 +267,8 @@ function OutfitItemList(props: { outfitItems: OutfitItem[] }) {
 							{count}.{" "}
 							{item.link ? <a href={item.link} target="_blank" className="">{item.color}{" "}{item.description} </a> : <span className="hover:cursor-not-allowed">{item.color}{" "}{item.description}</span>}
 						</h6>
-						{item.brand && <PSpan p={item.brand}  span="Brand" />}
-						{item.store && <PSpan p={item.store}  span="Store" />}
+						{item.brand && <PSpan p={item.brand} span="Brand" />}
+						{item.store && <PSpan p={item.store} span="Store" />}
 						<PSpan p={item.size ? item.size : "n/a"} span="Size" />
 						<PSpan p={item.price ? item.price : "n/a"} span="Price" />
 
