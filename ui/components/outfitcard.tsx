2056e88500be396afe4ea9326664e0461fe3f7f1
Steph <33664901+stxphcodes@users.noreply.github.com>
2023-12-20 12:17:28 -0500

    Issues 51-52: Make outfitcard consistent height and link to user closet in reviews (#54)
    * Config for next image
    
    * Lighthouse warning for font
    
    * Update sitemap
    
    * Make outfit card more consistent
    
    * Fix rating username link
    
    * Use nextlink and fix anchor tag for anonymous outfit poster
    
    * Zoom in on image to cover container
    
    ---------
    
    Co-authored-by: stxphcodes <sitesbystephanie@gmail.com>

--- ui/components/outfitcard.tsx
+++ ui/components/outfitcard.tsx
@@ -5,6 +5,7 @@ import { GetRatings, GetRatingsByOutfit, Rating } from '../apis/get_ratings';
 import { Modal } from './modals';
 import { PostRating } from '../apis/post_rating';
 import { VerifiedCheckIcon } from './icons/verified-check-icon';
+import Link from 'next/link';
 
 
 function Rating(props: { x: number, small?: boolean }) {
@@ -38,6 +39,7 @@ export function OutfitCard(props: {
 
 	const [allRatings, setAllRatings] = useState<Rating[] | null>(null)
 	const [readMore, setReadMore] = useState(false)
+
 
 	const handleSubmitRating = async (e: any) => {
 		e.preventDefault();
@@ -79,30 +81,25 @@ export function OutfitCard(props: {
 	return (
 		<>
 			<div className="w-72 shadow-md break-words">
-				<div className="px-2 py-1 bg-background text-sm " style={{fontFamily: 'custom-serif'}}>
-					{props.data.username ? <a className="flex flex-wrap items-center gap-1" href={`/closet/${props.data.username}`}>{props.data.username}&apos;s closet {props.verifiedBusiness && <VerifiedCheckIcon small={true}/>}</a> : "anonymous"}
-					
-					
+				<div className="px-2 py-1 bg-background text-sm " style={{ fontFamily: 'custom-serif' }}>
+					{props.data.username ? <Link href={`/closet/${props.data.username}`} passHref={true}>
+						<a className="flex flex-wrap items-center gap-1">{props.data.username}&apos;s closet {props.verifiedBusiness && <VerifiedCheckIcon small={true} />}</a>
+					</Link> : <div>anonymous</div>}
 					<div className="">{props.data.date}</div>
 				</div>
-
-				<img
-					onClick={() => setExpandImage(true)}
-					className="object-contain hover:cursor-pointer"
-					src={props.data.picture_url}
-				/>
-
+				<div className="overflow-hidden h-96 hover:cursor-pointer">
+					<img
+						onClick={() => setExpandImage(true)}
+						className="object-cover w-full h-full"
+						src={props.data.picture_url}
+					/>
+				</div>
 				<div className="p-2">
-					<h6 className="">{props.data.title}</h6>
-					<div className="flex gap-2 flex-wrap flex-row">
-						{props.data.style_tags.map((item) => (
-							<div className="" key={item}>{item}</div>
-						))}
-					</div>
+					<div style={{ fontFamily: 'custom-serif' }} className="whitespace-nowrap text-ellipsis overflow-hidden">{props.data.title}</div>
 					<div className="flex items-center mt-1">
-							<div className="hover:cursor-pointer mr-2" onClick={() => setExpandImage(true)}>
-								<Rating x={props.data.rating_average ? props.data.rating_average : 0} small={true}/>
-							</div>
+						<div className="hover:cursor-pointer mr-2" onClick={() => setExpandImage(true)}>
+							<Rating x={props.data.rating_average ? props.data.rating_average : 0} small={true} />
+						</div>
 						<a className="" onClick={() => setExpandImage(true)}>
 							{
 								!props.data.rating_count ? "no reviews submitted yet" : `from ${props.data.rating_count} ${props.data.rating_count > 1 ? "reviews" : "review"} `
@@ -110,7 +107,6 @@ export function OutfitCard(props: {
 						</a>
 					</div>
 				</div>
-
 
 				{!readMore ?
 					<div className="flex bg-background p-1 justify-center">
@@ -131,7 +127,6 @@ export function OutfitCard(props: {
 							}}>View less</a>
 						</div>
 					</>
-
 				}
 
 			</div>
@@ -171,7 +166,7 @@ export function OutfitCard(props: {
 								}
 
 								<div className="flex items-center">
-									<Rating x={props.data.rating_average ? props.data.rating_average : 0}/>
+									<Rating x={props.data.rating_average ? props.data.rating_average : 0} />
 									<div className="mx-2">
 										{
 											!props.data.rating_count ? "no reviews submitted yet" : `from ${props.data.rating_count} ${props.data.rating_count > 1 ? "reviews" : "review"} `
@@ -242,7 +237,10 @@ export function OutfitCard(props: {
 								{allRatings?.map(rating => {
 									return (
 										<div className="my-3" key={`${rating.outfit_id}-${rating.user_id}`}>
-											<div className="text-xs"><a >{rating.username ? rating.username : "anonymous"}</a> | {rating.date} </div>
+											<div className="text-xs">
+												{!rating.username ? <span className="text-primary">anonymous</span> : <Link href={`/closet/${rating.username}`} passHref={true}>
+													<a className="">{rating.username}</a>
+												</Link>} | {rating.date} </div>
 											<div className=""><span className="text-primary text-base pr-2">{rating.rating}</span>&quot;{rating.review}&quot;</div>
 										</div>
 									)
@@ -268,8 +266,8 @@ function OutfitItemList(props: { outfitItems: OutfitItem[] }) {
 							{count}.{" "}
 							{item.link ? <a href={item.link} target="_blank" className="">{item.color}{" "}{item.description} </a> : <span className="hover:cursor-not-allowed">{item.color}{" "}{item.description}</span>}
 						</h6>
-						{item.brand && <PSpan p={item.brand}  span="Brand" />}
-						{item.store && <PSpan p={item.store}  span="Store" />}
+						{item.brand && <PSpan p={item.brand} span="Brand" />}
+						{item.store && <PSpan p={item.store} span="Store" />}
 						<PSpan p={item.size ? item.size : "n/a"} span="Size" />
 						<PSpan p={item.price ? item.price : "n/a"} span="Price" />
 
