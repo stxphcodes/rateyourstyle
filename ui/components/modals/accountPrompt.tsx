d859fc5a0ded1b102806703b62094c471f605063
stxphcodes <sitesbystephanie@gmail.com>
2024-01-14 07:49:20 -0500

    Fix height

--- ui/components/modals/accountPrompt.tsx
+++ ui/components/modals/accountPrompt.tsx
@@ -1,13 +1,15 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { Modal } from "./";
 import { SignIn } from "./signin";
 import { CreateAccount } from "./createaccount";
+import Link from "next/link";
 export function AccountPromptModal(props: { clientServer: string }) {
     const [signInClicked, setSignInClicked] = useState<boolean>(false)
     const [createAccountClicked, setCreateAccountClicked] = useState<boolean>(false)
     return (
         <Modal handleClose={null} wideScreen={true} noPadding={true}>
-            <div className="h-96 py-96 items-center  justify-center flex">
+          
+            <div className="px-2 py-64 md:py-80  items-center  justify-center flex flex-wrap flex-row gap-4">
                 {
                     signInClicked &&
                     <SignIn
@@ -22,8 +24,18 @@ export function AccountPromptModal(props: { clientServer: string }) {
                 }
 
 
-                <h3 className="text-center">For unrestricted access, please <a onClick={() => setSignInClicked(!signInClicked)}>sign in</a> or <a onClick={() => setCreateAccountClicked(!createAccountClicked)}>create an account</a>.</h3>
+                <h3 className="text-center">
+                    To view this page, please{' '}
+                    <a onClick={() => setSignInClicked(!signInClicked)}>sign in</a>
+                    {' '}or {' '}
+                    <a onClick={() => setCreateAccountClicked(!createAccountClicked)}>create an account</a>.
+                </h3>
+
+                <h6 className="basis-full text-center">Go back to <Link href="/" passHref={true}><a>Home</a></Link></h6>
+
             </div>
+           
+
         </Modal>
 
     )
