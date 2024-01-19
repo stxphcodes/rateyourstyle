a66ea5de892c4fb0423bed4825f6bba747e2f6b2
stxphcodes <sitesbystephanie@gmail.com>
2024-01-19 00:15:37 -0500

    Add funcs to get box and whisker data points

--- ui/components/data-viz/box-and-whisker.tsx
+++ ui/components/data-viz/box-and-whisker.tsx
@@ -3,6 +3,41 @@ import ReactEcharts from "echarts-for-react";
 import { OutfitItem } from "../../apis/get_outfits";
 import { ExtractPrice } from "./pie-donut";
 
+function getBoxWhiskerDataPoints(sortedNumbers: number[]) {
+    // Calculate median (Q2)
+    const medianIndex = Math.floor(sortedNumbers.length / 2);
+    const median = sortedNumbers.length % 2 === 0
+        ? (sortedNumbers[medianIndex - 1] + sortedNumbers[medianIndex]) / 2
+        : sortedNumbers[medianIndex];
+
+    // Calculate first quartile (Q1)
+    const q1Index = Math.floor(sortedNumbers.length / 4);
+    const q1 = sortedNumbers.length % 4 === 0
+        ? (sortedNumbers[q1Index - 1] + sortedNumbers[q1Index]) / 2
+        : sortedNumbers[q1Index];
+
+    // Calculate third quartile (Q3)
+    const q3Index = Math.floor((3 * sortedNumbers.length) / 4);
+    const q3 = (sortedNumbers.length % 4 === 0 && q3Index < sortedNumbers.length)
+        ? (sortedNumbers[q3Index] + sortedNumbers[q3Index - 1]) / 2
+        : sortedNumbers[q3Index];
+
+     // Calculate interquartile range (IQR)
+     const iqr = q3 - q1;
+
+     // Calculate minimum and maximum within 1.5 * IQR from Q1 and Q3
+     const min = q1 - 1.5 * iqr;
+     const max = q3 + 1.5 * iqr;
+
+    // Return the box and whisker data points
+    return [
+        min,
+        q1,
+        median,
+        q3,
+        max
+    ]
+}
 
 export function ItemPricesChart(props: { items: OutfitItem[] }) { 
     var prices: number[] = []
@@ -19,6 +54,22 @@ export function ItemPricesChart(props: { items: OutfitItem[] }) {
     if (prices.length < 5) {
         return <h6 className="text-background-2">
             Please select 5 or more items *with prices* in the closet table to render this plot.</h6>
+    }
+
+    var dataPoints = getBoxWhiskerDataPoints(prices)
+    var calculatedMax = dataPoints[4]
+    var greaterThanMaxIndex = -1
+    var max = prices[prices.length-1]
+
+    for (let i=0; i< prices.length; i++) {
+        if (prices[i] > calculatedMax) {
+            greaterThanMaxIndex = i
+            break;
+        }
+    }
+
+    if (greaterThanMaxIndex > 0) {
+        max = prices[greaterThanMaxIndex ]
     }
 
     const option = {
@@ -86,7 +137,7 @@ export function ItemPricesChart(props: { items: OutfitItem[] }) {
                 show: false
             },
             min: prices[0],
-            max: prices[prices.length - 1]
+            max:  max
         },
         series: [
             {
