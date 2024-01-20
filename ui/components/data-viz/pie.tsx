fdb4880b34e3c21b8a4fc29441d9bfcd32627df4
stxphcodes <sitesbystephanie@gmail.com>
2024-01-20 10:51:21 -0500

    Word break for cost per wear

--- ui/components/data-viz/pie.tsx
+++ ui/components/data-viz/pie.tsx
@@ -15,16 +15,16 @@ export function ItemCostPerWear(props: { item: OutfitItem, count: number | undef
     var price = ExtractPrice(props.item.price)
     if (price < 0 || props.count == undefined) {
         return <h6 className="text-background-2 my-4">
-        Please select an item *with a price* in the closet table to render the Cost per Wear graph.</h6>
+        Please select an item *with a price* in the closet table to render the Cost Per Wear graph.</h6>
     }
 
-    var cpw = price/props.count
+    var cpw =  Math.round((price/props.count)*100/100);
 
     var data: any[] = []
     for (let i = 0; i < props.count; i++) {
         data.push({
             value: cpw,
-            name: `Cost per wear: ${cpw}`
+            name: `Cost\nper\nwear:\n${cpw}`
         })
     }
     const option = {
