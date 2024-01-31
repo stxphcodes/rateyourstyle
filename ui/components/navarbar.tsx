717d9446fc2ec2271a835ed0a52c97f7f0cb5039
stxphcodes <sitesbystephanie@gmail.com>
2024-01-31 11:47:08 -0500

    Update navbar menus:

--- ui/components/navarbar.tsx
+++ ui/components/navarbar.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-
+import { useRef } from "react"
 
 import { GetNotifications, Notification } from '../apis/get_notifications';
 import { GetOutfit, Outfit } from '../apis/get_outfits';
@@ -29,6 +29,8 @@ function cookieEnabled() {
 
 export function Navbar(props: { clientServer: string; cookie: string; user?: string; userNotifs?: UserNotifResp }) {
 	const router = useRouter();
+
+	const mobileMenuRef = useRef(null);
 
 	const [showSignInModal, setShowSignInModal] = useState<boolean>(false);
 
@@ -59,6 +61,15 @@ export function Navbar(props: { clientServer: string; cookie: string; user?: str
 		setDisplayMobileMenu(false)
 	}
 
+	const handleOutsideClick = (event: any) => {
+		if (
+			mobileMenuRef.current &&
+			!mobileMenuRef.current.contains(event.target)
+		) {
+			setDisplayMobileMenu(false)
+		}
+	};
+
 	useEffect(() => {
 		async function getusernotif() {
 			const resp = await GetUsernameAndNotifications(props.clientServer, props.cookie)
@@ -78,6 +89,11 @@ export function Navbar(props: { clientServer: string; cookie: string; user?: str
 
 		checkMobileScreenWidth(window);
 		window.addEventListener('resize', () => { checkMobileScreenWidth(window) });
+
+		document.addEventListener('mousedown', handleOutsideClick);
+		return () => {
+			document.removeEventListener('mousedown', handleOutsideClick);
+		};
 	}, []);
 
 	useEffect(() => {
@@ -95,6 +111,8 @@ export function Navbar(props: { clientServer: string; cookie: string; user?: str
 		}
 
 	}, [router])
+
+	
 
 	return (
 		<>
@@ -157,11 +175,7 @@ export function Navbar(props: { clientServer: string; cookie: string; user?: str
 				}
 			</div>
 			{displayMobileMenu &&
-				<div className="bg-background w-fit z-50 px-4 pb-8 h-full fixed top-8 left-0 flex flex-col gap-2 shadow text-lg" style={{ fontFamily: 'custom-serif' }}>
-					<div onClick={() => setDisplayMobileMenu(false)} className="self-end mt-4 hover:cursor-pointer">
-						&#10006;
-					</div>
-
+				<div className="bg-white shadow-xl border-t-2 border-background rounded w-fit z-50 px-4 pt-4 pb-8 h-full fixed top-8 left-0 flex flex-col gap-4 text-md" style={{ fontFamily: 'custom-serif' }} ref={mobileMenuRef}>
 					<Link href="/" passHref={true}>
 						<a className="">Home</a>
 					</Link>
@@ -214,17 +228,32 @@ export function Navbar(props: { clientServer: string; cookie: string; user?: str
 
 
 function NotificationMenu(props: { clientServer: string; cookie: string; handleClose: any, notifications: Notification[] }) {
+	const menuRef = useRef(null);
+
+	useEffect(() => {
+		document.addEventListener('mousedown', handleOutsideClick);
+		return () => {
+			document.removeEventListener('mousedown', handleOutsideClick);
+		};
+	}, []);
+
+	const handleOutsideClick = (event: any) => {
+		if (
+			menuRef.current &&
+			!menuRef.current.contains(event.target)
+		) {
+			props.handleClose()
+		}
+	};
+
 	return (
-		<div className="bg-background w-1/2 md:w-1/3 z-50 overflow-scroll fixed top-8 right-0 flex flex-col shadow text-lg max-h-screen" style={{ fontFamily: 'custom-serif' }}>
-			<div onClick={props.handleClose} className="self-end mt-4 hover:cursor-pointer pr-2">
-				&#10006;
-			</div>
+		<div className="bg-white border-t-2 border-background rounded-md w-1/2 md:w-1/3 z-50 overflow-scroll fixed top-8 sm:top-12 right-4 flex flex-col  shadow-xl text-lg max-h-screen" style={{ fontFamily: 'custom-serif' }} ref={menuRef}>
 			{props.notifications.length < 1 ?
 				<div className="text-background-2 text-xs p-2">
 					no notifications to show
 				</div> :
 				props.notifications.map((n) => (
-					<button className="p-2 border-b-2 hover:bg-white text-left overflow-clip shrink-0" onClick={() => {
+					<button className="px-2 py-3 hover:bg-background text-left overflow-clip shrink-0" onClick={() => {
 						let url = window.location.href;
 						url += `?outfit=${n.for_outfit_id}`
 						window.location.href = url;
@@ -244,11 +273,54 @@ function NotificationMenu(props: { clientServer: string; cookie: string; handleC
 	)
 }
 
+function UserMenu(props: { username: string, handleClose: any }) {
+	const menuRef = useRef(null);
+
+	useEffect(() => {
+		document.addEventListener('mousedown', handleOutsideClick);
+		return () => {
+			document.removeEventListener('mousedown', handleOutsideClick);
+		};
+	}, []);
+
+	const handleOutsideClick = (event: any) => {
+		if (
+			menuRef.current &&
+			!menuRef.current.contains(event.target)
+		) {
+			props.handleClose()
+		}
+	};
+
+	return (
+		<div className="bg-white border-t-2 border-background rounded-md z-50 overflow-scroll fixed top-8 sm:top-10 right-2 flex flex-col  shadow-xl text-lg max-h-screen" style={{ fontFamily: 'custom-serif' }} ref={menuRef}>
+			<Link href={`/user/${props.username}`} passHref={true}>
+				<button className="px-8 py-2  hover:bg-background text-left overflow-clip shrink-0 text-xs"
+				>
+					Account
+				</button>
+			</Link>
+			<button className="px-8 py-2  hover:bg-background text-left overflow-clip shrink-0 text-xs" onClick={() => {
+				// delete cookie
+				document.cookie = "rys-login=;expires=Thu, 01 Jan 1970 00:00:01 GMT"
+				// route back to home
+				window.location.href = "/"
+			}}
+			>
+				Logout
+			</button>
+		</div>
+	)
+}
+
 
 function UserAndNotification(props: { clientServer: string; cookie: string; username: string; hasNotifs: boolean }) {
-	const [displayMenu, setDisplayMenu] = useState(false);
+	const [displayNotifMenu, setDisplayNotifMenu] = useState(false);
+
+	const [displayUserMenu, setDisplayUserMenu] = useState(false);
 
 	const [notifs, setNotifs] = useState<Notification[]>([]);
+
 
 	useEffect(() => {
 		async function getnotifs() {
@@ -258,29 +330,36 @@ function UserAndNotification(props: { clientServer: string; cookie: string; user
 			}
 		}
 
-		if (displayMenu) {
+		if (displayNotifMenu) {
 			getnotifs()
 		}
 
-	}, [displayMenu])
+	}, [displayNotifMenu])
 
 	return (
 		<div className="flex flex-row gap-2 items-center">
-			<button className="hover:text-black" onClick={() => { setDisplayMenu(!displayMenu) }}>
+			<button className="hover:text-black" onClick={() => { setDisplayNotifMenu(!displayNotifMenu) }}>
 				{props.hasNotifs ? <NotificationFilledIcon /> :
 					<NotificationEmptyIcon />
 				}
 			</button>
-			<Link href={`/user/${props.username}`} passHref={true}>
-				<a>{props.username}</a>
-			</Link>
-			{displayMenu &&
+			<button className="text-primary hover:text-black" onClick={() => { setDisplayUserMenu(!displayUserMenu) }}>
+				{props.username}
+			</button>
+
+			{displayNotifMenu &&
+
 				<NotificationMenu
-					handleClose={() => { setDisplayMenu(false) }}
+					handleClose={() => { setDisplayNotifMenu(false) }}
 					notifications={notifs}
 					clientServer={props.clientServer}
 					cookie={props.cookie}
+
 				/>
+
+			}
+			{displayUserMenu &&
+				<UserMenu username={props.username} handleClose={()=>setDisplayUserMenu(false)} />
 			}
 		</div>
 
