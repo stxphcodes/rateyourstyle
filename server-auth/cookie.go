e4c9304e10bdcfb68c878012ce5d1115b9efdc40
stxphcodes <sitesbystephanie@gmail.com>
2024-07-21 15:26:57 -0400

    Remove unused

--- server-auth/cookie.go
+++ server-auth/cookie.go
@@ -2,38 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"net/http"
-	"strings"
-	"time"
 )
 
-const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 const cookieName = "rys-login"
-
-func uuid() string {
-	b := make([]byte, 16)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-
-	return string(b)
-}
-
-func getCookie(request *http.Request) (string, error) {
-	cookie := ""
-	for key, value := range request.Header {
-		if strings.ToLower(key) == cookieName {
-			cookie = value[0]
-		}
-	}
-
-	if cookie == "" {
-		return "", fmt.Errorf("rys-login header not found")
-	}
-
-	return cookie, nil
-}
 
 func createCookieStr(cookie string) string {
 	return fmt.Sprintf(
@@ -41,8 +12,4 @@ func createCookieStr(cookie string) string {
 		cookieName,
 		cookie,
 	)
-}
-
-func timeNow() string {
-	return time.Now().Format("2006-01-02") + "T" + time.Now().Format("15:04:05")
 }
