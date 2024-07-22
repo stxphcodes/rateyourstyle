0f594d8d682978574404bf988fc59c6af5b47eda
stxphcodes <sitesbystephanie@gmail.com>
2024-07-21 20:26:47 -0400

    UI build err

--- ui/components/forms/signup.tsx
+++ ui/components/forms/signup.tsx
@@ -171,9 +171,9 @@ const SignupForm = (props: { clientServer: string; authServer: string }) => {
                 }}
               />
               <label>
-                I agree to Rate Your Style's cookie policy. Rate Your Style uses
-                cookies to maintain your login session. The cookie is only used
-                for the purpose of saving your login details.
+                I agree to Rate Your Style&apos;s cookie policy. Rate Your Style
+                uses cookies to maintain your login session. The cookie is only
+                used for the purpose of saving your login details.
               </label>
             </div>
 
