83da95433f1b8f841b1e513d23ced3e19d3f5e4c
stxphcodes <sitesbystephanie@gmail.com>
2024-10-20 21:27:42 -0400

    Add option to make slider vertical

--- ui/components/base/sliders/dualSlider.tsx
+++ ui/components/base/sliders/dualSlider.tsx
@@ -6,28 +6,54 @@ export const DualSlider = (props: {
   value2: any;
   min: number;
   max: number;
+  vertical?: boolean;
 }) => (
-  <div className="flex gap-4 items-center mb-4">
+  <div className={`flex gap-4 ${!props.vertical ? "items-center" : ""} mb-4`}>
     <div className="w-fit">
       <label>{props.label}</label>
-      <div className="relative dual-slider">
+      <div
+        className={`relative dual-slider`}
+        style={props.vertical ? { marginLeft: "50%" } : {}}
+      >
         <input
+          style={
+            props.vertical
+              ? {
+                  writingMode: "vertical-lr",
+                  direction: "rtl",
+                  verticalAlign: "middle",
+                }
+              : {}
+          }
           id="dual-slider"
           type="range"
           min={props.min}
           max={props.max}
           step="1"
-          className="h-1 bg-gray-200 rounded-lg appearance-none cursor-pointer  p-0 m-0 absolute left-0"
+          className={`${
+            props.vertical ? "w-1" : "h-1"
+          } bg-gray-200 rounded-lg appearance-none cursor-pointer  p-0 m-0 absolute`}
           onChange={props.onChange1}
           value={props.value1}
         />
         <input
+          style={
+            props.vertical
+              ? {
+                  writingMode: "vertical-lr",
+                  direction: "rtl",
+                  verticalAlign: "middle",
+                }
+              : {}
+          }
           id="dual-slider"
           type="range"
           min={props.min}
           max={props.max}
           step="1"
-          className="h-1 bg-gray-200 rounded-lg appearance-none cursor-pointer  p-0 m-0 absolute left-0"
+          className={`${
+            props.vertical ? "w-1" : "h-1"
+          } bg-gray-200 rounded-lg appearance-none cursor-pointer  p-0 m-0 absolute`}
           onChange={props.onChange2}
           value={props.value2}
         />
